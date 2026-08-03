0802e59a3293a05a5009e4f41887143650869533
eocodn <eocodn1290@gmail.com>
2026-08-03 11:03:56 +0900

    fix(renderer): preserve status bar facade exports

--- src/renderer/src/components/status-bar/StatusBar.tsx
+++ src/renderer/src/components/status-bar/StatusBar.tsx
@@ -1,1 +1,8 @@
 export * from './status-bar-surface'
+export {
+  ClaudeSwitcherMenu,
+  CodexSwitcherMenu,
+  InlineUsageBars,
+  ProviderDetailsMenu,
+  ProviderSegment
+} from './status-bar-provider-menus'
